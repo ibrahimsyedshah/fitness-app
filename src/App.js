685002548c40e8eb6c955f8aab1fd124caee47dc
Ibrahim <isye6924@uni.sydney.edu.au>
2024-01-08 00:43:43 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import {Box} from '@mui/material';
+
+
+
+
+function App() {
+  return (
+    <Box width="400px">Navbar</Box>
+  )
+}
+
+export default App
